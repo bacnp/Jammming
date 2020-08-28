4052a167357c0a75b9ce46eee7b2b1236c23dcef
Nguyen Phuong Bac <68533404+bacnp@users.noreply.github.com>
2020-08-28 11:51:13 +0700

    Add-Tracks-to-a-Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -32,6 +32,14 @@ class App extends React.Component {
       id: 4
     }]
    }
+   this.addTrack = this.addTrack.bind(this);
+  }
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    this.state.playlistTracks.push(track)
+    this.setState ={playlistTracks: this.state.playlistTracks }
   }
   render () {
     return (
@@ -40,7 +48,7 @@ class App extends React.Component {
     <div className="App">
       <SearchBar />
       <div className="App-playlist">
-        <SearchResults searchResults={this.state.searchResults} />
+        <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
         <Playlist playlistName={this.state.playlistName} playlistTracks ={this.state.playlistTracks }  />
       </div>
     </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,6 +4,13 @@ import './TrackList.css';
 import Track from '../Track/Track.js';
 
 class TrackList extends React.Component {
+    constructor(props){
+        super(props);
+        this.addTrack = this.addTrack.bind(this);
+    }
+    addTrack() {
+        this.props.onAdd(this.props.track);
+    }
     render () {
         return (
             
@@ -12,7 +19,8 @@ class TrackList extends React.Component {
                this.props.tracks.map(track => {
                    return <Track 
                    track={track}
-                   key={track.id} />
+                   key={track.id}
+                   onAdd={this.props.onAdd} />
                })
             }
         </div>
